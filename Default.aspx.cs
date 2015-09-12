35865053d85696c54487bb554355308750799bbf
Sannanas <sanna.knoll@gmx.at>
2015-09-12 14:35:19 +0200

    Create Default.aspx.cs

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace index
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (TxtName.Text != "" && TxtPassword.Text != "")
+            {
+                Session["Name"] = TxtName.Text;
+                Session["Password"] = TxtPassword.Text;
+
+                Server.Transfer(null);
+
+            }
+        }
+
+        protected void BtnToRegister_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("register.aspx");
+        }
+    }
+}
