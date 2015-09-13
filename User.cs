da24e901563b9095cbb80880f72f5ea530da70ea
Zombii95 <thomas.irrer@hotmail.com>
2015-09-13 13:14:26 +0200

    Update User.cs

--- User.cs
+++ User.cs
@@ -31,7 +31,7 @@ namespace BO_Notify
         }
 
         public bool Create(){
-            string SQL = "insert into [User] ( name, password) values ( @nam, @pw)";
+            string SQL = "insert into [User] (UserId, name, password) values (@id, @nam, @pw)";
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = SQL;
             cmd.Connection = Main.GetConnection();
@@ -39,6 +39,7 @@ namespace BO_Notify
             mID = Guid.NewGuid().ToString();
 
             //cmd.Parameters.Add(new SqlParameter("id",mID));
+            cmd.Parameters.Add(new SqlParameter("id", mID));
             cmd.Parameters.Add(new SqlParameter("nam",mUsername));
             cmd.Parameters.Add(new SqlParameter("pw", mPassword));
 
@@ -50,16 +51,37 @@ namespace BO_Notify
             User oneUser = new User();
             oneUser.ID = reader.GetString(0);
             oneUser.Username = reader.GetString(1);
+            oneUser.Password = reader.GetString(2);
             return oneUser;
         
         }
 
-        internal static User Load(string UserID){
-            string SQL = "select ID, name from User where ID = @id";
+        public User Login(string username){
+            string SQL = "select * from [User] where name = @name";
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = SQL;
             cmd.Connection = Main.GetConnection();
-            cmd.Parameters.Add(new SqlParameter("id", UserID));
+            cmd.Parameters.Add(new SqlParameter("name", username));
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                return fillUserFromSQLDataReader(reader);
+
+            }
+            else
+            {
+                return null;
+            }
+           
+        }
+        
+        internal static User Load(string UserId){
+            string SQL = "select ID, name from User where name = @nam";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = SQL;
+            cmd.Connection = Main.GetConnection();
+            cmd.Parameters.Add(new SqlParameter("id", UserId));
             SqlDataReader reader = cmd.ExecuteReader();
             if(reader.HasRows){
                 reader.Read();
