0059059415a76355a257574b47d109cd6111756e
Zombii95 <thomas.irrer@hotmail.com>
2015-09-13 13:13:55 +0200

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,24 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BO_Notify;
 
 namespace index
 {
     public partial class index : System.Web.UI.Page
     {
+        User currentUser;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            currentUser = Main.newUser();
         }
 
         protected void BtnLogin_Click(object sender, EventArgs e)
         {
             if (TxtName.Text != "" && TxtPassword.Text != "")
             {
-                Session["Name"] = TxtName.Text;
-                Session["Password"] = TxtPassword.Text;
+                currentUser.Username = TxtName.Text;
+                currentUser.Password = TxtPassword.Text;
 
-                Server.Transfer(null);
+                if((currentUser.Login(currentUser.Username).Username).Equals(currentUser.Username) &&
+                        (currentUser.Login(currentUser.Username).Password).Equals(currentUser.Password)){
+                    Session["name"]= currentUser.Username;
+                    Session["ID"] = currentUser.Login(currentUser.Username).ID;
+                    Response.Redirect("Page1.aspx");
+                }else{
+                    lblError.Text = "MISTAKE!!!!!";
+                }
+
+                //Server.Transfer(null);
 
             }
         }
